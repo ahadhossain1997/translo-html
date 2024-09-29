6bdcd374fdd073dfdd0394331600705fcbc68410
ahad hossain <wpahad1997@gmail.com>
2024-09-29 23:28:07 +0600

    techin home one running

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
 "use strict";
 
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 (function ($) {
   "use strict";
 
@@ -315,32 +319,14 @@
     /*--------------------------------------------------------------
     techin BRAND SLIDER INIT
     --------------------------------------------------------------*/
-    var techin_brand_slider = $('.techin-brand-slider');
-    if (techin_brand_slider.is_exist()) {
-      techin_brand_slider.slick({
-        infinite: true,
-        slidesToShow: 5,
+    var brand_slider = $('.techin-brand-slider');
+    if (brand_slider.is_exist()) {
+      brand_slider.slick(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty({
+        slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: false,
-        dots: false,
-        autoplay: true,
-        autoplaySpeed: 0,
-        speed: 6000,
-        cssEase: 'linear',
-        pauseOnHover: true,
-        adaptiveHeight: true,
-        responsive: [{
-          breakpoint: 1199,
-          settings: {
-            slidesToShow: 3
-          }
-        }, {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 2
-          }
-        }]
-      });
+        autoplay: false,
+        arrows: true
+      }, "slidesToScroll", 1), "infinite", true), "speed", 1000), "lazyLoad", 'progressive'), "prevArrow", '<button class="slide-arrow techin-brand-next"></button>'), "nextArrow", '<button class="slide-arrow techin-brand-prev"></button>')).slickAnimation();
     }
     var popup_youtube = $('.video-init');
     if (popup_youtube.is_exist()) {
